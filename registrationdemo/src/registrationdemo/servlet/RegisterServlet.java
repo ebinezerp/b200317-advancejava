84f489b0fb2e1febe8b940a455771002f623541d
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2020-06-15 12:23:04 +0530

    generic and http servlet and explained about reqeust types

---
+++ registrationdemo/src/registrationdemo/servlet/RegisterServlet.java
@@ -0,0 +1,21 @@
+package registrationdemo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().print("request reached");
+
+	}
+
+}
